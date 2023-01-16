eb012466a221e45aabdf25663c2dd3b0d0dd31a6
TIME <opout777@yandex.ru>
2023-01-16 15:59:08 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -10,6 +11,18 @@ public class Main {
             System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен " + age + " то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+
+        int term = 10;
+        if (term > 5) {
+            System.out.println("На улице " + term + " градусов, можно идти без шапки");
+        } else {
+            System.out.println("На улице " + term + " градусов, нужно надеть шапку");
         }
     }
 }
